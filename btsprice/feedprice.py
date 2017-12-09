82a3fc89af39cf370ae03acab301340088bb5c66
xiangxn <xiangxn@163.com>
2017-12-10 01:07:09 +0800

    Set the black swan check switch

--- btsprice/feedprice.py
+++ btsprice/feedprice.py
@@ -244,7 +244,7 @@ class FeedPrice(object):
         for asset in asset_list:
             if asset not in real_price:
                 continue
-            if self.feedapi.is_blackswan(asset):
+            if self.config["price_limit"]["check_blackswan"] == 1 and self.feedapi.is_blackswan(asset):
                 continue
             if asset not in my_feeds:
                 need_publish[asset] = real_price[asset]
